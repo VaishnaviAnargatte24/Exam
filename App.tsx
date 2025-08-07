8459dec044edb8d500a08e7f77025718bb3305a3
PrajwalThorve <prajwalthorve@gtasterix.com>
2025-08-07 11:35:05 +0530

    HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
+
 const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
